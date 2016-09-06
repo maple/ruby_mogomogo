6e790c3d6ac20e074526d097d5eb9b6cdc6df007
fortunan <maplewine+program@gmail.com>
2016-09-06 14:37:12 +0900

    support multi files in current directory

--- photos/timestamp_fromfilename.rb
+++ photos/timestamp_fromfilename.rb
@@ -12,23 +12,44 @@ def updateTimestampFromNameofDropbox (name:, ext:)
 
   ## remove extention & replace words.
   mtimesrc = name.dup
-  mtimesrc.slice!("." + ext) #.gsub!(".", ":")
+  p mtimesrc
+  mtimesrc.slice!(/[\w\s\d\/]+\//)
+  mtimesrc.slice!("." + ext)
   mtimesrc.gsub!(".", ":")
   p mtimesrc
-
   mt = Time.parse(mtimesrc)
 
   p at
   p mt
 
-  #File::utime(at, mt, fname)
+  File::utime(at, mt, name)
 end
   
-# sample file of Dropbox
-fname = "2016-08-28 10.36.50.jpg"
+# enable to find in sub-directories.
+def create_filename_list (param)
+  ar = []
+  Dir::glob(param){|f|
+    next unless FileTest.file?(f)
+    #ar << "#{File.basename(f)} : #{File::stat(f).size}"
+    ar << f
+  }
+  return ar
+end
+
+#####
+# make a list by searching *.ext
+# get current directory
+
+# extension
 ext = "jpg"
 
-updateTimestampFromNameofDropbox(name: fname, ext: ext)
+location = Dir::pwd
 
+# pick up files
+param_of_search = location + "/**/*" + ext
 
+filelist = create_filename_list param_of_search
 
+filelist.each { |f|
+  updateTimestampFromNameofDropbox(name: f, ext: ext)
+}
