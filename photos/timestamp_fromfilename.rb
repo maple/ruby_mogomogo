edbb2bb0c843089ed542ae69a52f03688701e93f
fortunan <maplewine+program@gmail.com>
2016-09-06 14:10:54 +0900

    make a new function from base code.

--- photos/timestamp_fromfilename.rb
+++ photos/timestamp_fromfilename.rb
@@ -2,32 +2,33 @@
 
 require 'time'
 
+def updateTimestampFromNameofDropbox (name:, ext:)
+
+  s = File.stat(name)
+  p s.atime # latest accessed time.
+  p s.mtime # latest updated time.
+
+  at = s.atime
+
+  ## remove extention & replace words.
+  mtimesrc = name.dup
+  mtimesrc.slice!("." + ext) #.gsub!(".", ":")
+  mtimesrc.gsub!(".", ":")
+  p mtimesrc
+
+  mt = Time.parse(mtimesrc)
+
+  p at
+  p mt
+
+  #File::utime(at, mt, fname)
+end
+  
 # sample file of Dropbox
 fname = "2016-08-28 10.36.50.jpg"
 ext = "jpg"
 
-s = File.stat(fname)
-p s.atime # latest accessed time.
-p s.mtime # latest updated time.
-
-
-
-# modify timestamp.
-
-at = s.atime
-
-## remove extention.
-mtimesrc = fname.dup
-mtimesrc.slice!("." + ext)
-mtimesrc.gsub!(".", ":")
-#p mtimesrc
-
-mt = Time.parse(mtimesrc)
-
-p mt
-p at
-
-# File::utime(at, mt, fname)
+updateTimestampFromNameofDropbox(name: fname, ext: ext)
 
 
 
