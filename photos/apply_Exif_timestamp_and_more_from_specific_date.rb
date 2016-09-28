fe755d62c8bdee4b0cfaa6b3ac560640f30d644e
maple <maplewine+github@gmail.com>
2016-09-28 16:07:56 +0900

    add new file to process other exif tags.

---
+++ photos/apply_Exif_timestamp_and_more_from_specific_date.rb
@@ -0,0 +1,74 @@
+#!~/.rbenv/shims/ruby
+### ---------------------------------
+# need to install gem of mini_exiftool & exiftool & others.
+# 
+# gem install mini_exiftool
+#   Also install exiftool http://www.sno.phy.queensu.ca/~phil/exiftool/
+# gem install rmagick
+#    Also you need to install ImageMagick. Such as "brew install imagemagick"
+#
+#
+## usage:
+# $ ruby apply_Exif_timestamp_from_specific_date.rb [time]
+# if user doesn't put argument as a date, this program uses current time.
+#
+# 
+## background
+# I have a lot of scan data to keep my memory as digital data.
+# such as photos of paper, pictures my children drew, brochure of a traveling and so on.
+# I noticed I'm almost forgetting when I took these photos, when my son drew or when my family has visited that place of the brochure.
+# To keep information of timestamp will help me in the future, therefore I've written this code. ;)
+#
+
+require "mini_exiftool"
+
+def set_timestamp_to_exiftags (time: , file: , width:, height: )
+  photo = MiniExiftool.new file.to_s
+  # p photo.date_time_original
+  photo.date_time_original = time
+  photo.date_time = time
+  photo.date_time_digitized = time
+
+  begin
+    photo.save!
+    puts "done to rewrite exif timestamp : (#{file})"
+  rescue => e
+    puts e
+  end
+end
+
+# create filelist
+
+def create_filename_list (param)
+  ar = []
+  Dir::glob(param.upcase){|f|
+    next unless FileTest.file?(f)
+    #ar << "#{File.basename(f)} : #{File::stat(f).size}"
+    ar << f
+  }
+  Dir::glob(param.capitalize){|f|
+    next unless FileTest.file?(f)
+    ar << f
+  }
+  return ar
+end
+
+# set start time.
+ARGV[0] ? date = ARGV[0] : date = Time.now.to_s
+
+# extension
+ext = "jpg"
+location = Dir::pwd
+
+# pick up files
+param_of_search = location + "/**/*" + ext
+
+filelist = create_filename_list param_of_search
+
+localtime = Time.parse(date)
+
+filelist.each { |f|
+  set_timestamp_to_exiftags file:f, time: localtime, width: 0, height: 0
+  File::utime(localtime, localtime, f)
+  localtime = localtime + 10
+}
