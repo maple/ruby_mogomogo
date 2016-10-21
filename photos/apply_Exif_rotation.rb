69ea40e6e9f978177c70cf7b67cddce2e8650128
maple <maple._wine@csc.jp>
2016-10-21 15:00:05 +0900

    add new feature to modify orienation of Exif.

--- photos/apply_Exif_rotation.rb
+++ photos/apply_Exif_rotation.rb
@@ -6,8 +6,16 @@ require "mini_exiftool"
 def set_orientation_to_exiftags (rt: , file: )
   p file
   photo = MiniExiftool.new file.to_s
-  #p photo.orientation
+  p photo.orientation
   # GPSMapDatum
+  # photo.orientation ="Rotate 270 CW"
+  # photo.orientation ="Rotate 90 CW"
+  photo.orientation ="Rotate 180"
+
+  # check width / height
+  if photo.image_width > photo.image_height
+    
+  end
   
   begin
     # photo.save!
@@ -31,10 +39,6 @@ def create_filename_list (param)
   return ar
 end
 
-
-photo = MiniExiftool.new "testfile".to_s
-
-exit
 
 # extension
 ext = ".jpg"
